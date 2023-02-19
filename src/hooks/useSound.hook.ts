b6753b251505762ca81888af2ee0db6d1a6d6e6c
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-02-19 19:02:59 +0100

    removed web-audio-api

--- src/hooks/useSound.hook.ts
+++ src/hooks/useSound.hook.ts
@@ -1,25 +1,25 @@
-import { AudioContext } from "web-audio-api";
-
-// export const useSound = (sound: string) => {
-//   const soundAudio = new Audio(sound);
-//   soundAudio.preload = "auto";
-//   return {
-//     play: () => {
-//       soundAudio.play();
-//     },
-//   };
-// };
+// import { AudioContext } from "web-audio-api";
 
 export const useSound = (sound: string) => {
-  const audioContext = new AudioContext();
   const soundAudio = new Audio(sound);
-  const audioSource = audioContext.createMediaElementSource(soundAudio);
-  audioSource.connect(audioContext.destination);
   soundAudio.preload = "auto";
-
   return {
     play: () => {
       soundAudio.play();
     },
   };
 };
+
+// export const useSound = (sound: string) => {
+//   const audioContext = new AudioContext();
+//   const soundAudio = new Audio(sound);
+//   const audioSource = audioContext.createMediaElementSource(soundAudio);
+//   audioSource.connect(audioContext.destination);
+//   soundAudio.preload = "auto";
+
+//   return {
+//     play: () => {
+//       soundAudio.play();
+//     },
+//   };
+// };
