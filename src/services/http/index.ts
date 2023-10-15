2f05abec9faf47a27d6b76034754bd5e537fd1ce
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-10-15 14:51:28 +0200

    deploy

--- src/services/http/index.ts
+++ src/services/http/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "https://api.sereni-fy.com:8000";
+export const API_URL = "https://api.sereni-fy.com";
+// export const API_URL = "http://localhost:8000";
 
 export const $api = axios.create({
   baseURL: API_URL,
